dc9b0d4bd1d16eb24178ce65a79ba4a3f0732b3d
Ando Roots <ando@sqroot.eu>
2018-12-23 02:14:07 +0200

    Add more error handling

--- monitor.py
+++ monitor.py
@@ -1,16 +1,20 @@
 import logging
 import requests
 from notify import alert
-
+from  requests.exceptions import RequestException
 logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
 
 
 def check_git(domain):
     url = 'http://{}/.git/HEAD'.format(domain)
-    response = requests.head(url, timeout=7, headers={'User-Agent':'Webmon Research Agent (https://github.com/anroots/webmon)'})
     logging.info('Scanning {} for .git/HEAD'.format(domain))
 
-    if response.status_code == 200:
+    try:
+        response = requests.head(url, timeout=7, headers={'User-Agent':'Webmon Research Agent (https://github.com/anroots/webmon)'})
+    except RequestException:
+        return
+
+    if response.status_code == 200 and 'refs' in response.text:
         message = 'Found Git folder at {0}'.format(domain)
         logging.info(message)
         alert(message)
--- notify.py
+++ notify.py
@@ -2,5 +2,5 @@ import requests
 import os
 
 def alert(message):
-    url = 'https://fleep.io/hook/{0}'.format(os.environ('FLEEP_HOOK'))
+    url = 'https://fleep.io/hook/{0}'.format(os.environ.get('FLEEP_HOOK'))
     requests.post(url,json={message: message})
